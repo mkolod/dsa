d6901bb5975fa6a237ce935fb9322c6d3b10b045
Marek Kolodziej <mkolod@gmail.com>
2014-01-20 10:34:07 -0500

    Updated test suites

--- src/test/scala/us/marek/ds/FindMedianTestSuite.scala
+++ src/test/scala/us/marek/ds/FindMedianTestSuite.scala
@@ -25,28 +25,3 @@ class FindMedianTestSuite extends FlatSpec {
   }
    
 }
-  /*
-  def fixture = new {
-    val builder = new StringBuilder("ScalaTest is ")
-    val buffer = new ListBuffer[String]
-  }
-  */
- 
-
-/*
-object FindMedianTestSuite {
-
-  /**
-   * main method for demo
-   */
-  def main(args: Array[String]): Unit = {
-
-    // print array contents
-    def prettyPrint[T](a: Array[T]) = s"Array(${a.mkString(",")})"
-
-    val arr1 = Array(1, 5, 4, 3, 2)
-    val arr2 = arr1 :+ 6
-    println(s"Array 1 (odd length): ${prettyPrint(arr1)}, median = ${median(arr1)}")
-    println(s"Array 2 (even length): ${prettyPrint(arr2)}, median = ${median(arr2)}")
-  }
-*/
